739a2ce5a2dc951c77d3b08febf459e6b3fa1b8e
nermin50 <nerminsaar@gmail.com>
2021-08-05 23:41:31 +0300

    DataBase Connection yapıldı, DataContext sınıfı oluşturuldu , EntityFramework package'i yüklendi

---
+++ RealEstateAspNetMVC5_Staj2021/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RealEstateAspNetMVC5_Staj2021.Models
+{
+    public class DataContext : DbContext
+    {
+        //Database Connection 
+
+        public DataContext() : base ("defaultCon")
+        {
+
+        }
+    }
+}
